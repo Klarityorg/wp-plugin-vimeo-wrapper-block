fcd9ff83fb4591241c33c9bda6e67d707053d64f
Bruno Perel <bruno@klarity.org>
2019-03-05 14:46:23 +0100

    Bump version

--- plugin.php
+++ plugin.php
@@ -5,7 +5,7 @@
  * Description: Klarity Vimeo wrapper block
  * Author: Klarity
  * Author URI: https://github.com/Klarityorg
- * Version: 1.0.3
+ * Version: 1.0.4
  * License: GPL2+
  * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
  *
